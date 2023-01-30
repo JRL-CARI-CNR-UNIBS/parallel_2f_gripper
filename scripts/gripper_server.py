3b5402ac6a648bee0ed7ed8f92bb3e24dd1fb2aa
Michele Delledonne <m.delledonne002@unibs.it>
2023-01-30 09:58:33 +0100

    one gear joint version, ready for tests

--- scripts/gripper_server.py
+++ scripts/gripper_server.py
@@ -36,24 +36,32 @@ from pybullet_utils.srv import ChangeControlMode
 from parallel_2f_gripper.srv import MoveGripper
 
 
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+def green_p(msg):
+    print('\033[92m' + msg + '\033[0m')
+
+
+def red_p(msg):
+    print('\033[91m' + msg + '\033[0m')
+
+
+def blue_p(msg):
+    print('\033[94m' + msg + '\033[0m')
+
+
+def yellow_p(msg):
+    print('\033[93m' + msg + '\033[0m')
+
+
+def cyan_p(msg):
+    print('\033[96m' + msg + '\033[0m')
 
 
 def move_gripper(srv, name, position, velocity, effort, joint_target_publish_rate, change_mode_srv):
     mm_max = 218
     mm_zero_joint = 100
     joint_max_val = 1.00
-    joint_min_val = -0.80
-    tollerance = 0.1
+    joint_min_val = -0.76
+    tollerance = 0.01
 
     control_mode = srv.control_mode
     target_position = srv.position
@@ -61,27 +69,29 @@ def move_gripper(srv, name, position, velocity, effort, joint_target_publish_rat
     target_torque = srv.torque
 
     if (control_mode != 'position' and control_mode != 'velocity' and control_mode != 'torque' and control_mode != 'open' and control_mode != 'close'):
-        print('Wrong control mode')
+        red_p('Wrong control mode')
         return 'false'
     else:
         print('type: ' + control_mode)
     if (control_mode == 'open'):
-        target_position = mm_max
+#        target_position = mm_max
+        target_position = joint_max_val
         print('Mode: open. Target_position: ' + str(target_position))
 
     if (control_mode == 'close'):
-        target_position = 0
+#        target_position = 0
+        target_position = joint_min_val
         print('Mode: close. Target_position: ' + str(target_position))
 
     if (control_mode == 'position'):
         if not target_position:
-            print('Position control mode require a target position')
+            red_p('Position control mode require a target position')
             return 'false'
         print('Mode: position. Target_position: ' + str(target_position))
 
     if (control_mode == 'torque'):
         if not target_torque:
-            print('Torque control mode require a target torque')
+            red_p('Torque control mode require a target torque')
             return 'false'
         if (target_torque > 0.0):
             target_position = 0
@@ -91,7 +101,7 @@ def move_gripper(srv, name, position, velocity, effort, joint_target_publish_rat
 
     if (control_mode == 'velocity'):
         if not target_velocity:
-            print('Velocty control mode require a target velocity')
+            red_p('Velocty control mode require a target velocity')
             return 'false'
         if (target_velocity > 0.0):
             target_position = 0
@@ -107,7 +117,7 @@ def move_gripper(srv, name, position, velocity, effort, joint_target_publish_rat
     print('             Target_torque: ' + str(target_torque))
 
     if (change_mode_srv(['parallel_gripper'], 'position') == 'false'):
-        print('error with change_control_mode service')
+        red_p('error with change_control_mode service')
         raise 'false'
 
     current_joint_state = rospy.wait_for_message('/parallel_gripper/joint_states', JointState)
@@ -116,40 +126,53 @@ def move_gripper(srv, name, position, velocity, effort, joint_target_publish_rat
 
     finish = False
     if (control_mode == 'close'):
-        dx = -abs(target_velocity) * (1 / joint_target_publish_rate)
+        dx = -abs(target_velocity) / joint_target_publish_rate
     elif (control_mode == 'open'):
-        dx = abs(target_velocity) * (1 / joint_target_publish_rate)
+        dx = abs(target_velocity) / joint_target_publish_rate
     else:
         if (current_mm < target_position):
-            dx = abs(target_velocity) * (1 / joint_target_publish_rate)
+            dx = abs(target_velocity) / joint_target_publish_rate
         else:
-            dx = -abs(target_velocity) * (1 / joint_target_publish_rate)
+            dx = -abs(target_velocity) / joint_target_publish_rate
     position[0] = current_position
     rate = rospy.Rate(joint_target_publish_rate)
     while not finish:
-        current_mm += dx
-        if (current_mm > mm_max):
-            current_mm -= dx
+#        current_mm += dx
+#        if (current_mm > mm_max):
+#            current_mm -= dx
+#            finish = True
+#            print('Joint limit')
+
+        current_position += dx
+        if (current_position > joint_max_val):
+            current_position -= dx
             finish = True
             print('Joint limit')
-        position[0] = numpy.arcsin(((current_mm - mm_zero_joint) / 2) / 70)
+        position[0] = current_position
+#        position[0] = numpy.arcsin(((current_mm - mm_zero_joint) / 2) / 70)
         real_js = rospy.wait_for_message('/parallel_gripper/joint_states', JointState)
         real_position = real_js.position[real_js.name.index('right_finger_joint')]
-        real_mm = 100 + 2 * 70 * numpy.sin(real_position)
+#        real_mm = 100 + 2 * 70 * numpy.sin(real_position)
         print(real_position)
-        print(real_mm)
+#        print(real_mm)
         real_torque = real_js.effort[real_js.name.index('right_finger_joint')]
-
-        if (real_position < joint_min_val or real_position > joint_max_val):
+        red_p(str(real_torque))
+        if ((real_position < joint_min_val and dx < 0) or (real_position > joint_max_val and dx > 0)):
             finish = True
             print('Joint limit')
-        elif (dx < 0 and real_mm < target_position + tollerance):
+#        elif (dx < 0 and real_mm < target_position + tollerance):
+#            finish = True
+#            print('Target position')
+#        elif (dx > 0 and real_mm > target_position - tollerance):
+#            finish = True
+#            print('Target position')
+        elif (dx < 0 and real_position < target_position + tollerance):
             finish = True
             print('Target position')
-        elif (dx > 0 and real_mm > target_position - tollerance):
+        elif (dx > 0 and real_position > target_position - tollerance):
             finish = True
             print('Target position')
-        elif (real_torque > (target_torque * 2)):
+        elif (abs(real_torque) > (target_torque * 2)):
             finish = True
             print('Target_torque')
         rate.sleep()
@@ -177,7 +200,7 @@ def main():
     if (rospy.has_param('/joint_target_publish_rate')):
         joint_target_publish_rate = rospy.get_param('/joint_target_publish_rate')
     else:
-        print('/joint_target_publish_rate pasam not set')
+        red_p('/joint_target_publish_rate pasam not set')
         raise SystemExit
 
     print('Wait for service change_control_mode...')
@@ -185,7 +208,7 @@ def main():
     print('Connected to /change_control_mode')
     change_mode_srv = rospy.ServiceProxy('change_control_mode', ChangeControlMode)
     if (change_mode_srv(['parallel_gripper'], 'position') == 'false'):
-        print('error with change_control_mode service')
+        red_p('error with change_control_mode service')
         raise SystemExit
 
     initial_joint_state = rospy.wait_for_message('/parallel_gripper/joint_states', JointState)
@@ -217,7 +240,7 @@ def main():
     rospy.Service("/move_parallel_gripper", MoveGripper, lambda msg: move_gripper(msg, name, position, velocity, effort, joint_target_publish_rate, change_mode_srv))
 
     rospy.spin()
-
+    jt_pub_thread.join()
 
 if __name__ == '__main__':
     main()
